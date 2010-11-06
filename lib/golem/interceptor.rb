f9b5b9ff75f5b05c3eac9b35105be921a6a37f60
Nathan Witmer <nwitmer@gmail.com>
2010-11-06 17:50:37 -0600

    Prooftest of interception: set time with /time in chat

--- lib/golem/interceptor.rb
+++ lib/golem/interceptor.rb
@@ -12,13 +12,35 @@ module Golem
       puts "proxying new connection"
       @server_parser = Parser.new
       @client_parser = Parser.new(false)
+      @time = 0
+    end
+
+    def unbind
+      puts "client disconnected, shutting down"
+      EM.stop
     end
 
     def receive_data(data)
       to_send = ""
       client_parser.parse(data).each do |packet|
+        if packet.kind == :chat && packet.values.first =~ /\/time (\d+|dawn|sunrise|dusk|sunset|noon|midnight)/
+          case $1
+          when "dawn", "sunrise"
+            @time = 23000
+          when "dusk", "sunset"
+            @time = 13000
+          when "noon"
+            @time = 6000
+          when "midnight"
+            @time = 18000
+          else
+            @time = $1.to_i
+          end
+          tell_client "setting time to #{$1}"
+        else
         # puts "<-- #{packet.inspect}"
-        to_send << packet.raw
+          to_send << packet.raw
+        end
       end
       server.send_data to_send unless to_send.empty?
     end
@@ -30,10 +52,18 @@ module Golem
     def from_server(data)
       to_send = ""
       server_parser.parse(data).each do |packet|
+        if packet.kind == :update_time
+          to_send << Packet.server_packets_by_kind[:update_time].new(@time).encode
+        else
+          to_send << packet.raw
+        end
         # puts "--> #{packet.inspect}"
-        to_send << packet.raw
       end
       send_data to_send unless to_send.empty?
+    end
+
+    def tell_client(msg)
+      send_data Packet.server_packets_by_kind[:chat].new("<proxy> #{msg}").encode
     end
 
   end
@@ -44,6 +74,10 @@ module Golem
     end
     def receive_data(data)
       @client.from_server(data)
+    end
+    def unbind
+      puts "lost connection to server"
+      EM.stop
     end
   end
 end
